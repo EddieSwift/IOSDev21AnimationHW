b7a2e8b7eb6e8a3f60eea1638a56eb047d874063
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-10-17 17:48:51 +0300

    Initial Commit

---
+++ IOSDev21AnimationHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDev21AnimationHW
+//
+//  Created by Eduard Galchenko on 10/17/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSDev21AnimationHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDev21AnimationHW
+//
+//  Created by Eduard Galchenko on 10/17/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
